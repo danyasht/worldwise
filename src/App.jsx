434b5de3b7b70f226a650fdd401e5509b446283d
MacBook Danya <macbook@MacBook-Air.local>
2026-02-17 20:22:07 +0200

    Initial commit: worldwise app

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { lazy, Suspense } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+import { CitiesProvider } from './context/CitiesContext';
+import { AuthProvider } from './context/FakeAuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
+
+import CityList from './components/CityList';
+import CountryList from './components/CountryList';
+import City from './components/City';
+import Form from './components/Form';
+import SpinnerFullPage from './components/SpinnerFullPage';
+
+const Homepage = lazy(() => import('./pages/Homepage'));
+const Pricing = lazy(() => import('./pages/Pricing'));
+const AppLayout = lazy(() => import('./pages/AppLayout'));
+const Login = lazy(() => import('./pages/Login'));
+const Product = lazy(() => import('./pages/Product'));
+
+function App() {
+  return (
+    <CitiesProvider>
+      <AuthProvider>
+        <BrowserRouter>
+          <Suspense fallback={<SpinnerFullPage />}>
+            <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="pricing" element={<Pricing />} />
+              <Route path="product" element={<Product />} />
+
+              <Route path="login" element={<Login />} />
+
+              <Route
+                path="app"
+                element={
+                  <ProtectedRoute>
+                    <AppLayout />
+                  </ProtectedRoute>
+                }
+              >
+                <Route index element={<Navigate replace to="cities" />} />
+
+                <Route path="cities" element={<CityList />} />
+                <Route path="cities/:id" element={<City />} />
+                <Route path="countries" element={<CountryList />} />
+                <Route path="form" element={<Form />} />
+              </Route>
+            </Routes>
+          </Suspense>
+        </BrowserRouter>
+      </AuthProvider>
+    </CitiesProvider>
+  );
+}
+
+export default App;
---
+++ src/context/CitiesContext.jsx
@@ -0,0 +1,167 @@
+import {
+  createContext,
+  useEffect,
+  useContext,
+  useReducer,
+  useCallback,
+} from 'react';
+
+const BASE_URL = 'http://localhost:9000';
+
+const CitiesContext = createContext();
+
+const initialState = {
+  cities: [],
+  isLoading: false,
+  currentCity: {},
+  error: '',
+};
+
+function reducer(state, action) {
+  const { type, payload } = action;
+  switch (type) {
+    case 'loading': {
+      return { ...state, isLoading: true };
+    }
+
+    case 'cities/loaded': {
+      return { ...state, isLoading: false, cities: payload };
+    }
+
+    case 'city/loaded': {
+      return { ...state, isLoading: false, currentCity: payload };
+    }
+
+    case 'city/created': {
+      return {
+        ...state,
+        isLoading: false,
+        cities: [...state.cities, payload],
+        currentCity: payload,
+      };
+    }
+
+    case 'city/deleted': {
+      return {
+        ...state,
+        isLoading: false,
+        cities: state.cities.filter((city) => city.id !== payload),
+        currentCity: {},
+      };
+    }
+
+    case 'rejected': {
+      return { ...state, isLoading: false, error: payload };
+    }
+
+    default: {
+      throw new Error('Unknown action:(');
+    }
+  }
+}
+
+function CitiesProvider({ children }) {
+  const [{ cities, isLoading, currentCity, error }, dispatch] = useReducer(
+    reducer,
+    initialState,
+  );
+
+  useEffect(function () {
+    async function fetchCities() {
+      dispatch({ type: 'loading' });
+
+      try {
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        dispatch({ type: 'cities/loaded', payload: data });
+      } catch {
+        dispatch({
+          type: 'rejected',
+          payload: 'There was an error fetching data!',
+        });
+      }
+    }
+    fetchCities();
+  }, []);
+
+  const getCity = useCallback(
+    async function getCity(id) {
+      if (Number(id) === currentCity.id) return;
+
+      dispatch({ type: 'loading' });
+
+      try {
+        const res = await fetch(`${BASE_URL}/cities/${id}`);
+        const data = await res.json();
+        dispatch({ type: 'city/loaded', payload: data });
+      } catch {
+        dispatch({
+          type: 'rejected',
+          payload: 'There was an error fetching data!',
+        });
+      }
+    },
+    [currentCity.id],
+  );
+
+  async function addCity(newCity) {
+    dispatch({ type: 'loading' });
+    try {
+      const res = await fetch(`${BASE_URL}/cities`, {
+        method: 'POST',
+        body: JSON.stringify(newCity),
+        headers: {
+          'Content-type': 'application/json',
+        },
+      });
+      const data = await res.json();
+      dispatch({ type: 'city/created', payload: data });
+    } catch {
+      dispatch({
+        type: 'rejected',
+        payload: 'There was an error creating city!',
+      });
+    }
+  }
+
+  async function deleteCity(id) {
+    dispatch({ type: 'loading' });
+    try {
+      await fetch(`${BASE_URL}/cities/${id}`, {
+        method: 'DELETE',
+      });
+
+      dispatch({ type: 'city/deleted', payload: id });
+    } catch {
+      dispatch({
+        type: 'rejected',
+        payload: 'There was an error deleting city!',
+      });
+    }
+  }
+
+  return (
+    <CitiesContext.Provider
+      value={{
+        cities,
+        isLoading,
+        currentCity,
+        getCity,
+        addCity,
+        deleteCity,
+        error,
+      }}
+    >
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined)
+    throw new Error('CitiesContext was used outside the CitiesProvider');
+  return context;
+}
+
+export { CitiesProvider, useCities };
